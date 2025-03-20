f41c0eac5db06d7917621c5f035da4e7e5ef3856
Rajat Pandit <rajat.pandit@nimapinfotech.com>
2025-03-20 09:45:17 +0530

    initial comment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Csharrpday3{
+
+    public class Program{
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+           
+            Console.ReadKey();
+        }
+    }
+
+}
